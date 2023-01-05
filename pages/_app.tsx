56c72434041accc8749fc97d4475770347468924
Rikard Johansson <106700975+Rikard-Johansson97@users.noreply.github.com>
2023-01-05 14:12:54 +0100

    task-1: set up project environment (#1)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { setContext } from '@apollo/client/link/context';
+import { ApolloClient,createHttpLink, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const token = 'e75dd9f5b446cb431814bda5c6adb8';
+const httpLink = createHttpLink({
+  uri: 'https://graphql.datocms.com/',
+});
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: Object.assign(
+      headers || {},
+      {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      }
+    )
+  }
+});
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <ApolloProvider client={client}><Component {...pageProps} /></ApolloProvider>
 }
