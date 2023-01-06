a0c064ef7a16a29905ad4821c53f21e0b7b47d5b
Rikard Johansson <Rikard.com@hotmail.com>
2023-01-06 15:30:11 +0100

    Footer component
    created footer component that holds 5 links and copyright text.

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Link from "next/link";
+import styles from "./footer.module.scss";
+import { IconButton } from "@mui/material";
+
+const Footer = () => (
+  <footer className={styles.footer}>
+    <Link href='/' className={styles.logo}>
+      Consid Commerce
+    </Link>
+    <nav className={styles.nav}>
+      <IconButton>
+        <Link href='/' className={styles.link}>
+          Home
+        </Link>
+      </IconButton>
+      <IconButton>
+        <Link href='/product' className={styles.link}>
+          Products
+        </Link>
+      </IconButton>
+      <IconButton>
+        <Link href='/about' className={styles.link}>
+          About
+        </Link>
+      </IconButton>
+      <IconButton>
+        <Link href='/contact' className={styles.link}>
+          Contact
+        </Link>
+      </IconButton>
+    </nav>
+    <p className={styles.copyright}>Copyright 2023. All rights reserved.</p>
+  </footer>
+);
+
+export default Footer;
