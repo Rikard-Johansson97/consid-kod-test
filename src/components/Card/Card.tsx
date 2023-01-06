a66804ba7f353b49435d1fd7243cb303ef91d685
Rikard Johansson <106700975+Rikard-Johansson97@users.noreply.github.com>
2023-01-06 15:34:13 +0100

    Card component (#8)
    created card component that holds information about products

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,43 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import styles from "./card.module.scss";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import IconButton from "@mui/material/IconButton";
+import NewReleasesIcon from "@mui/icons-material/NewReleases";
+import Link from "next/link";
+
+const Card = (props: any) => {
+  console.log(props);
+  return (
+    <Link href={`/product/${props.id}`} className={styles.card}>
+      <div className={styles.cardImg}>
+        {props.new && (
+          <NewReleasesIcon fontSize='large' className={styles.newRelease} />
+        )}
+        <img src={props.mainImage.url} alt={props.name} />
+      </div>
+      <div className={styles.cardText}>
+        <p>{props.name}</p>
+        <h4>{props.name}</h4>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non,
+          eligendi.
+        </p>
+        <div className={styles.cardPrice}>
+          <p>{props.price} kr</p>
+          <div className='cardIcons'>
+            <IconButton>
+              <FavoriteBorderIcon />
+            </IconButton>
+            <IconButton>
+              <ShoppingCartOutlinedIcon />
+            </IconButton>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default Card;
