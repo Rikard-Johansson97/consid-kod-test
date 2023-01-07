390219fb502ce0f7627cebb188e3304e6993c238
Rikard Johansson <Rikard.com@hotmail.com>
2023-01-07 17:16:32 +0100

    Contact page
    created contact page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,7 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
-
+import { useQuery } from "@apollo/client";
+import { GET_ALL_POSTS } from "../../src/graphql/queries";
+import Navbar from "../../src/components/Navbar/Navbar";
+import Banner from "../../src/components/Banner/Banner";
 const Contact = () => {
-  return <div>Contact</div>;
+  const { loading, error, data } = useQuery(GET_ALL_POSTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <Navbar />
+      <Banner data={data.allPages[1]} />
+    </div>
+  );
 };
 
 export default Contact;
