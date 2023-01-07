ee57149daadf9c366e4f526294b3955731de6093
Rikard Johansson <106700975+Rikard-Johansson97@users.noreply.github.com>
2023-01-07 17:19:27 +0100

    About page (#10)
    created about page route

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,7 +1,23 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
-
+import { useQuery } from "@apollo/client";
+import { GET_ALL_POSTS } from "../../src/graphql/queries";
+import Navbar from "../../src/components/Navbar/Navbar";
+import Banner from "../../src/components/Banner/Banner";
 const About = () => {
-  return <div>About</div>;
+  const { loading, error, data } = useQuery(GET_ALL_POSTS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  console.log(data.allPages[0]);
+
+  return (
+    <div>
+      <Navbar />
+      <Banner data={data.allPages[0]} />
+    </div>
+  );
 };
 
 export default About;
--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -5,18 +5,12 @@ import Button from "@mui/material/Button";
 import Link from "next/link";
 
 const Banner = (props: any) => {
-  console.log(props);
+  const data = props.data;
   return (
     <div className={styles.banner}>
-      <img
-        src={props.startpage.mainImage.url}
-        alt={props.startpage.mainImage.url}
-      />
+      <img src={data.mainImage.url} alt={data.mainImage.url} />
       <div className={styles.text}>
-        <h1>{props.startpage.title}</h1>
-        <Link href='/product'>
-          <Button variant='outlined'>SHOP</Button>
-        </Link>
+        <h1>{data.title}</h1>
       </div>
     </div>
   );
