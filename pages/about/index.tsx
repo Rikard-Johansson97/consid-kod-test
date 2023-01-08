6867a162f2776767ffa7d6206aaaa59875bc7cfc
Rikard Johansson <106700975+Rikard-Johansson97@users.noreply.github.com>
2023-01-08 22:26:33 +0100

    Fixed things (#15)

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "@apollo/client";
 import { GET_ALL_POSTS } from "../../src/graphql/queries";
 import Navbar from "../../src/components/Navbar/Navbar";
 import Banner from "../../src/components/Banner/Banner";
+import CardWrapper from "../../src/components/CardWrapper/CardWrapper";
 const About = () => {
   const { loading, error, data } = useQuery(GET_ALL_POSTS);
 
@@ -16,6 +17,36 @@ const About = () => {
     <div>
       <Navbar />
       <Banner data={data.allPages[0]} />
+      <CardWrapper>
+        <div className='about'>
+          <h1>About Consid</h1>
+          <p>
+            Consid is a leading e-commerce company that provides a wide range of
+            products to customers around the world. Our mission is to make
+            shopping easy and convenient for everyone, by offering a wide
+            selection of products at competitive prices, and delivering them
+            quickly and reliably.
+          </p>
+          <h2>Our Website</h2>
+          <p>
+            Our website, Consid.com, is designed to be user-friendly and easy to
+            navigate. We offer a wide range of products in categories such as
+            electronics, home goods, fashion, and more. Customers can easily
+            search for products, add them to their cart, and checkout securely.
+            We also offer fast and reliable shipping options to ensure that our
+            customers receive their orders as quickly as possible.
+          </p>
+          <h2>Our Values</h2>
+          <ul>
+            <li>Customer satisfaction is our top priority</li>
+            <li>
+              We strive to offer the best prices and value to our customers
+            </li>
+            <li>We are committed to providing high-quality products</li>
+            <li>We believe in ethical and sustainable business practices</li>
+          </ul>
+        </div>
+      </CardWrapper>
     </div>
   );
 };
--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "@apollo/client";
 import { GET_ALL_POSTS } from "../../src/graphql/queries";
 import Navbar from "../../src/components/Navbar/Navbar";
 import Banner from "../../src/components/Banner/Banner";
+import CardWrapper from "../../src/components/CardWrapper/CardWrapper";
 const Contact = () => {
   const { loading, error, data } = useQuery(GET_ALL_POSTS);
 
@@ -14,6 +15,20 @@ const Contact = () => {
     <div>
       <Navbar />
       <Banner data={data.allPages[1]} />
+      <CardWrapper>
+        <form>
+          <label htmlFor='name'>Name:</label>
+          <input type='text' id='name' />
+          <br />
+          <label htmlFor='email'>Email:</label>
+          <input type='email' id='email' />
+          <br />
+          <label htmlFor='message'>Message:</label>
+          <textarea id='message'></textarea>
+          <br />
+          <button type='submit'>Send</button>
+        </form>
+      </CardWrapper>
     </div>
   );
 };
