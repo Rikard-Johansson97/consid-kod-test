d68137035d1aff3c95d6ea9f38ca14ba0b699eed
Rikard Johansson <106700975+Rikard-Johansson97@users.noreply.github.com>
2023-01-12 10:58:56 +0100

    About StructuredText
    * About StructuredText
    
    * Structured Text in About page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -5,11 +5,12 @@ import { GET_PAGE_CONTENT } from "../../src/graphql/queries";
 import Navbar from "../../src/components/Navbar/Navbar";
 import Banner from "../../src/components/Banner/Banner";
 import CardWrapper from "../../src/components/CardWrapper/CardWrapper";
+import { StructuredText } from "react-datocms";
 const About = () => {
   const { loading, error, data } = useQuery(GET_PAGE_CONTENT, {
     variables: { slug: "about" },
   });
-
+  console.log(data);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
@@ -18,34 +19,8 @@ const About = () => {
       <Navbar />
       <Banner {...data.page} />
       <CardWrapper>
-        <div className='about'>
-          <h1>About Consid</h1>
-          <p>
-            Consid is a leading e-commerce company that provides a wide range of
-            products to customers around the world. Our mission is to make
-            shopping easy and convenient for everyone, by offering a wide
-            selection of products at competitive prices, and delivering them
-            quickly and reliably.
-          </p>
-          <h2>Our Website</h2>
-          <p>
-            Our website, Consid.com, is designed to be user-friendly and easy to
-            navigate. We offer a wide range of products in categories such as
-            electronics, home goods, fashion, and more. Customers can easily
-            search for products, add them to their cart, and checkout securely.
-            We also offer fast and reliable shipping options to ensure that our
-            customers receive their orders as quickly as possible.
-          </p>
-          <h2>Our Values</h2>
-          <ul>
-            <li>Customer satisfaction is our top priority</li>
-            <li>
-              We strive to offer the best prices and value to our customers
-            </li>
-            <li>We are committed to providing high-quality products</li>
-            <li>We believe in ethical and sustainable business practices</li>
-          </ul>
-        </div>
+        <h2>About Consid</h2>
+        <StructuredText data={data.page.content} />
       </CardWrapper>
     </div>
   );
